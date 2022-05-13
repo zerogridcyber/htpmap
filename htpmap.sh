692c92a19a00d6a753caf321e61a1517b5b864c7
zerogridcyber <105462055+zerogridcyber@users.noreply.github.com>
2022-05-13 16:59:25 -0400

    Add files via upload

---
+++ htpmap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Banner
+R='\033[1;31m'
+G='\033[1;32m'
+Y='\033[1;33m'
+blue='\033[1;34m'
+magenta='\033[1;35m'
+cyan='\033[1;36m'
+W='\033[1;37m'
+Green='\033[32m'
+Gren='\033[32m'
+Gris='\033[90m'
+
+echo $(clear)
+echo -e "$blue
+           . .IIIII             .II
+  IIIIIII. I  II  .    II..IIIIIIIIIIIIIIIIIIII
+ .  .IIIIII  II          III$cyan  Hack$blue IIIIIIIIII.
+    .IIIII.III I      IIIIIIIII$cyan The$blue IIIIIIIIII  I
+   .IIIIII$cyan Hack$blue II  .IIII$cyan Planet$blue IIIIIIIII. III
+    IIIIIII$cyan  The$blue   ' IIIII I IIIIIIIIIIII III I
+    .II$cyan     Planet$blue   IIIIIIIIIIII  IIIIIIIIII
+       I.           .IIIIIIIIIIII   I   II  I
+         .IIII        IIIIIIIIIIII     .       I
+          IIIII.          IIIIII           . I.
+         IIIIIIIII         IIIII             ..I  II .
+          IIIIIII          IIII..             IICII
+            IIII           III. I            IIIEIII
+            III             I                I  IHI
+             II     $cyan  [-]$magenta WhiteHat$cyan [-]$blue        D   .
+             I            $magenta World$reset 
+             \n"
+echo
